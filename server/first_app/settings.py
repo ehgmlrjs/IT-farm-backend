ab5c29d9446d40a78ae5ffca18cfe5c4b04e807c
ehgmlrjs <ehgmlrjs@naver.com>
2023-11-14 16:39:28 +0900

    aa

--- server/first_app/settings.py
+++ server/first_app/settings.py
@@ -158,7 +158,10 @@ REST_USE_JWT = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES':[
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 
