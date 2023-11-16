84fa4812ea716bcd615a9d17b1c08d5ccca40aa4
ehgmlrjs <ehgmlrjs@naver.com>
2023-11-16 19:31:16 +0900

    aa

--- server/order/urls.py
+++ server/order/urls.py
@@ -3,7 +3,7 @@ from .views import *
 urlpatterns = [
     path('create/', OrderCreateView.as_view()),
     path('read/<int:order_id>/', OrderReadView.as_view()),
-    path('pay/<int:order_id>/', OrderPayView.as_view()),
+    # path('pay/<int:order_id>/', OrderPayView.as_view()),
     path('delete/<int:order_id>/',OrderDeleteView.as_view()),
     path('review/create/', ReviewCreateView.as_view()),
     path('review/update/', ReviewUpdateView.as_view()),
