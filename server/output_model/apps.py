58b80cdf72518ee0991295cbc8af06d004425371
ehgmlrjs <ehgmlrjs@naver.com>
2023-11-20 12:44:44 +0900

    Perf

--- server/output_model/apps.py
+++ server/output_model/apps.py
@@ -6,13 +6,13 @@ class OutputModelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'output_model'
 
-    model_path = os.path.join(os.path.dirname(__file__), 'C:/IT-farm-backend/server/output_model/files/output_model.joblib')
+    model_path = './output_model/files/output_model.joblib'
     model = None
 
-    sc_X_path = os.path.join(os.path.dirname(__file__), 'C:/IT-farm-backend/server/output_model/files/sc_X.joblib')
+    sc_X_path = './output_model/files/sc_X.joblib'
     sc_X = None
     
-    sc_y_path = os.path.join(os.path.dirname(__file__), 'C:/IT-farm-backend/server/output_model/files/sc_y.joblib')
+    sc_y_path = './output_model/files/sc_y.joblib'
     sc_y = None
 
 
